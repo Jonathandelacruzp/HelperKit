d463467bf95f7351a6d707c74b946f95e7d06c45
Jonathan De La Cruz <jonathan.delacruzp@outlook.com.pe>
2022-02-21 22:31:07 -0500

    changed date provider interface

---
+++ src/HelperKit/HelperKit/DateTimeProvider.cs
@@ -0,0 +1,55 @@
+﻿namespace HelperKit;
+
+public static partial class Extensions
+{
+    /// <summary>
+    /// Convert time between TimeProviders
+    /// </summary>
+    /// <param name="fromProvider"></param>
+    /// <param name="toProvider"></param>
+    /// <returns></returns>
+    public static DateTime ConvertTime(this IDateTimeProvider fromProvider, IDateTimeProvider toProvider)
+    {
+        var dateUnspecified = DateTime.SpecifyKind(fromProvider.Now, DateTimeKind.Unspecified);
+        return TimeZoneInfo.ConvertTime(dateUnspecified, fromProvider.TimeZoneInfo, toProvider.TimeZoneInfo);
+    }
+
+    /// <summary>
+    /// Convert time between TimeProviders
+    /// </summary>
+    /// <param name="date"></param>
+    /// <param name="fromProvider"></param>
+    /// <param name="toProvider"></param>
+    /// <returns></returns>
+    public static DateTime ConvertTime(this DateTime date, IDateTimeProvider fromProvider, IDateTimeProvider toProvider)
+    {
+        var dateUnspecified = DateTime.SpecifyKind(date, DateTimeKind.Unspecified);
+        return TimeZoneInfo.ConvertTime(dateUnspecified, fromProvider.TimeZoneInfo, toProvider.TimeZoneInfo);
+    }
+}
+
+public sealed class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime Now => DateTime.UtcNow.Add(TimeZoneInfo.BaseUtcOffset);
+    public TimeZoneInfo TimeZoneInfo { get; }
+
+    private DateTimeProvider(TimeZoneInfo timeZoneInfo) => TimeZoneInfo = timeZoneInfo;
+
+    public static class Factory
+    {
+        public static IDateTimeProvider Create(TimeZoneInfo timeZoneInfo)
+        => new DateTimeProvider(timeZoneInfo);
+
+        public static IDateTimeProvider Create(string id, TimeSpan baseUtcOffset, string standardName = null, string displayStandardName = null)
+            => new DateTimeProvider(TimeZoneInfo.CreateCustomTimeZone(id, baseUtcOffset, standardName ?? id, displayStandardName ?? standardName ?? id));
+
+        public static IDateTimeProvider Create(string timeZoneId)
+            => SystemDateTimeProviders.TryGetValue(timeZoneId, out var value)
+                ? value
+                : throw new Exception($"The value is provided in not valid timeZoneId: {timeZoneId}");
+    }
+
+    private static readonly ReadOnlyDictionary<string, IDateTimeProvider> SystemDateTimeProviders = new(TimeZoneInfo.GetSystemTimeZones().ToDictionary<TimeZoneInfo, string, IDateTimeProvider>(x => x.Id, x => new DateTimeProvider(x)));
+
+    public static IDictionary<string, IDateTimeProvider> GetSystemDateTimeProviders() => SystemDateTimeProviders;
+}
---
+++ src/HelperKit/HelperKit/Interfaces/IDateTimeProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace HelperKit.Interfaces;
+
+/// <summary>
+/// Defines the structure of a time provider, Makes easier the unit testing and value mock
+/// </summary>
+public interface IDateTimeProvider
+{
+    public DateTime Now { get; }
+    public TimeZoneInfo TimeZoneInfo { get; }
+}

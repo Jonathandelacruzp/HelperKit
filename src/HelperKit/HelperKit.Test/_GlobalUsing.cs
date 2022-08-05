a8c438c395545665c84c66ed9c4ffac7aae7adfc
Jonathan De La Cruz <jonathan.delacruzp@outlook.com.pe>
2022-08-04 22:13:37 -0500

    Added SafeFireAndForger extension Helper Added CloneObject extension

--- src/HelperKit/HelperKit.Test/_GlobalUsing.cs
+++ src/HelperKit/HelperKit.Test/_GlobalUsing.cs
@@ -1,8 +1,5 @@
 ﻿global using FluentAssertions;
 global using HelperKit.Interfaces;
-global using System;
-global using System.Collections.Generic;
+global using HelperKit.Test.Models;
 global using System.Globalization;
-global using System.IO;
-global using System.Linq;
 global using Xunit;
